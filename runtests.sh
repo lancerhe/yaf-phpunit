065b6fec19a48db310f8147444619686f023b2ec
Lancer He <lancer.he@gmail.com>
2015-12-13 13:37:56 +0800

    update phpunit to version 4.4.* and runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -11,12 +11,15 @@ for folder in "public" "application" "tests"; do
     echo -n "Checking syntax for *.php in ./${folder}, "
     error_output=`find "./${folder}" -type f -iname "*.php" -exec ${PHP_BIN} -l {} \; | grep -i "Errors.parsing"`
     if [ "$error_output" != "" ]; then
-        echo "failed!"
-        echo $error_output
+        echo -e "\033[41;37mfailed!\033[0m"
+        echo -e "\033[41;37m$error_output\033[0m"
         exit 1
     else
         echo "OK!"
     fi
 done
 
-${PHP_BIN} ./vendor/bin/phpunit -c tests/phpunit.xml
+echo -e "\r\n\033[43;37mRun unit tests.\033[0m"
+${PHP_BIN} ./vendor/bin/phpunit -c tests/phpunit.xml --testsuite unit
+echo -e "\r\n\033[43;37mRun acceptance tests.\033[0m"
+${PHP_BIN} ./vendor/bin/phpunit -c tests/phpunit.xml --testsuite acceptance
